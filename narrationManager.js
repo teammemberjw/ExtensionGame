8f81812e2e325781183e45519ac33101e1dabeeb
Edesau <magictyper@live.com>
2017-05-01 15:14:19 -0700

    Update narrationManager.js

--- narrationManager.js
+++ narrationManager.js
@@ -1,3 +1,17 @@
+/*
+Goals (functionality): 
+1) Accept arrays of Strings to present in sequence
+2) Change or close the narration box when the user clicks 
+3) Trigger callback function when narration is finished
+4) Allow placement of box to be configured
+5) Allow font to be configured
+
+Goals (appearance): 
+1) Create divs specified to present the narration
+<s>2) Create old-timey font for the look</s>
+3) Created style of background of box
+*/
+
 function makeNarrationManager(){
   /* PRIVATE VARIABLES */
   
