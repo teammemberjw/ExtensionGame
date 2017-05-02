d9a463c87e37feddb12fb3df4afef0be13ccb496
Edesau <magictyper@live.com>
2017-05-02 16:03:27 -0700

    Update narrationManager.js

--- narrationManager.js
+++ narrationManager.js
@@ -5,15 +5,18 @@ function makeNarrationManager(){
   
   /* PUBLIC METHODS */
   var that = {
-    advanceDialogue: function(){
-     if(dialogue.length!=0 && index<dialogue.length-1){
-       index++;
-       return dialogue[index-1];
-     }else{
-       index = 0;
-       return null; // when the calling function receives null, it should interpret this as "there is no more dialogue"
-     }
-   },
+    advanceText: function(){
+       if(dialogue.length!=0 && index<dialogue.length-1){
+         index++;
+        // return dialogue[index-1]; send this data to the boxPainter
+       }else{
+         index = 0;
+       //  return null; // when the calling function receives null, it should interpret this as "there is no more dialogue"
+       }
+    },
+    select: function(choice){
+      /* LOGIC GOES HERE */
+    }
   };
   return that;
 }
