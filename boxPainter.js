e4656021fbc51dab866f3c541fd879d61959e22d
Edesau <magictyper@live.com>
2017-05-02 15:57:04 -0700

    added goals
    in a hopefully more intuitive manner

--- boxPainter.js
+++ boxPainter.js
@@ -1,17 +1,3 @@
-/*
-Goals (functionality): 
-1) -COMPLETE- 
-2) Change or close the narration box when the user clicks 
-3) Trigger callback function when narration is finished ***** need clarification
-4) Allow placement of box to be configured
-5) Allow font to be configured ****************************** won't this be in the css file?
-
-Goals (appearance): 
-1) Create divs specified to present the narration
-2) -COMPLETE- 
-3) Create style of background of box
-*/
-
 function makeBoxPainter(){
  /* PRIVATE VARIABLES */
  var dialogue; // the current String to display
@@ -22,22 +8,20 @@ function makeBoxPainter(){
  
  /* PUBLIC METHODS */
  var that = {
-   drawBox: function(dialogueArr, x, y){
-     dialogue = dialogueArr;
-     boxX = x; 
-     boxY = y;
-     /* CALCULATE WIDTH AND HEIGHT HERE */
-   },
-   advanceDialogue: function(){
-     if(dialogue.length!=0 && index<dialogue.length-1){
-       index++;
-       return dialogue[index-1];
-     }else{
-       index = 0;
-       return null; // when the calling function receives null, it should interpret this as "there is no more dialogue"
-     }
-   },
-   
+  setLocation: function(x, y){
+   boxX = x;
+   boxY = y;
+  },
+  calculateSize: function(text, x, y){
+    dialogue = text;
+    boxX = x; 
+    boxY = y;
+    /* CALCULATE WIDTH AND HEIGHT HERE */
+  },
+  display: function(){
+   /* CLEAR LAST BOX, CREATE A NEW ONE */
+   /* WRITE TEXT */
+  }
  };
  return that;
 }
