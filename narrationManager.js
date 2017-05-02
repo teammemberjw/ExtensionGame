045f8330d89225a642522b54837a2326458b74cd
Edesau <magictyper@live.com>
2017-05-02 15:47:31 -0700

    shuffled code around

--- narrationManager.js
+++ narrationManager.js
@@ -1,10 +1,19 @@
 function makeNarrationManager(){
   /* PRIVATE VARIABLES */
-  
+  var dialogue = {};
+  var index = 0;
   
   /* PUBLIC METHODS */
   var that = {
-    
+    advanceDialogue: function(){
+     if(dialogue.length!=0 && index<dialogue.length-1){
+       index++;
+       return dialogue[index-1];
+     }else{
+       index = 0;
+       return null; // when the calling function receives null, it should interpret this as "there is no more dialogue"
+     }
+   },
   };
   return that;
 }
