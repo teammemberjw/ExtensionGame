29756fd4d010a9adecba7a0d7a191085318976cd
Edesau <magictyper@live.com>
2017-05-01 20:25:45 -0700

    added displayDialogue

--- narrationManager.js
+++ narrationManager.js
@@ -1,6 +1,6 @@
 /*
 Goals (functionality): 
-1) Accept arrays of Strings to present in sequence
+-COMPLETE- 1) Accept arrays of Strings to present in sequence 
 2) Change or close the narration box when the user clicks 
 3) Trigger callback function when narration is finished
 4) Allow placement of box to be configured
@@ -8,25 +8,37 @@ Goals (functionality):
 
 Goals (appearance): 
 1) Create divs specified to present the narration
-2) Create old-timey font for the look -COMPLETE- 
+-COMPLETE- 2) Create old-timey font for the look -- Will be used in the CSS file
 3) Create style of background of box
 */
 
 function makeNarrationManager(){
   /* PRIVATE VARIABLES */
-  var font; // the name and location of the desired .tff file
-  var dialogue = {};
+  var dialogue = {}; // each separate "page" is a String; ie ["Hello. <br>It's good to see you.", "How are you?"];
+  var index = 0; // the current position of displayed dialogue
   var boxX;
   var boxY;
+  var boxW; // Note: once the size of the font can be ascertained, this will be calculated based on 
+  var boxH; // the width and height of the letters
   
   /* PUBLIC METHODS */
   var that = {
-    init: function(dialogueArr, x, y, fontLocation){
+    drawBox: function(dialogueArr, x, y){
       dialogue = dialogueArr;
       boxX = x; 
       boxY = y;
-      font = fontLocation + ".tff";
+      /* CALCULATE WIDTH AND HEIGHT HERE */
+    },
+    displayDialogue: function(){
+      if(dialogue.length!=0 && index<dialogue.length-1){
+        index++;
+        return dialogue[index-1];
+      }else{
+        index = 0;
+        return null; // when the calling function receives null, it should interpret this as "there is no more dialogue"
+      }
     }
-  } // makeNarrationManager
+  } // that
+ } // makeNarrationManager
   return that;
 } // narrationManager
