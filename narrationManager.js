9d1a045c0416a0663f7187ce2d4ba60df2e7c9dd
Edesau <magictyper@live.com>
2017-05-05 16:15:45 -0700

    added some test dialogue

--- narrationManager.js
+++ narrationManager.js
@@ -1,6 +1,6 @@
 function makeNarrationManager(){
   /* PRIVATE VARIABLES */
-  var dialogue = {};
+  var dialogue = ["This is a test!"]; // test dialogue
   var index = 0;
   var boxPainter = boxPainter.makeBoxPainter();
   var portraitPainter = portraitPainter.makePortraitPainter();
@@ -9,7 +9,7 @@ function makeNarrationManager(){
   var that = {
     advanceText: function(){
       if(dialogue.length!=0 && index<dialogue.length-1){
-        boxPainter.calculateSize(dialogue[index], /* X VALUE GOES HERE */, /* Y VALUE GOES HERE */);
+        boxPainter.calculateSize(dialogue[index], 10, 10); // the x and y locations of the box
         index++;
       }else{
         boxPainter.calculateSize(null, 0, 0);
